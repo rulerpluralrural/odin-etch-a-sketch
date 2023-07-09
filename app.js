152b1e76db3e9109c62274195fd9ce83dc5795fd
Gerald Javier <33073866+rulerpluralrural@users.noreply.github.com>
2023-07-09 17:39:41 +0800

    "Added CSS styling"
    * added buttons that will be the color of the brush on click
    * added an input type color that can pick different color for the brush
    * added input type range that will determine the size of the sketch pad
    * added and eraser button and reset button

--- app.js
+++ app.js
@@ -1,11 +1,27 @@
+// sketch pad container variable
 const container = document.getElementById('container');
+
+// color picker variable
+const colorPicker = document.getElementById('color-picker');
+
+// sketch pad size slider variable
+const slider = document.getElementById('slider');
+
+// Buttons variables
+const blackBtn = document.getElementById('btn-black');
+const grayBtn = document.getElementById('btn-gray');
+const rainbowBtn = document.getElementById('btn-rainbow');
+const eraseBtn = document.getElementById('btn-erase');
+const resetBtn = document.getElementById('btn-reset');
+
+
 const div = [];
 
 // create 16x16 div and append to the container
 for (let i = 0; i < 256; i++) {
     const divs = document.createElement('div');
     container.appendChild(divs);
-    divs.className = ('sketch-box')
+    container.style.gridTemplateColumns = 'repeat(16,1fr)';
     div.push(divs);
 }
 
