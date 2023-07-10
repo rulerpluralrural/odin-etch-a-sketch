149bcc7ac55f2976d0467936aca7e9182573f6b0
Gerald Javier <33073866+rulerpluralrural@users.noreply.github.com>
2023-07-10 18:26:40 +0800

    " added coloring functions and clear function "
    * button on click will use color coresponding it
    * added random colors to the rainbow button
    * eraser button will use white color
    * clear button will make all the divs background to white
    * color picked on input type color will be used
    * add click event on the body to have control on the mouseover coloring

--- app.js
+++ app.js
@@ -1,30 +1,36 @@
 // sketch pad container variable
 const container = document.getElementById('container');
 
-// sketch pad size slider variable
-const slider = document.getElementById('slider');
+// variable that will show whatever picked in the input type color
+const colorPicker_span = document.getElementById('color-picked');
+
+// variable that will show the size of the sketchpad based on the input type range
 const spanTxt = document.querySelector('#size');
 
-// Buttons variables
-const blackBtn = document.querySelector('#btn-black');
-const grayBtn = document.querySelector('#btn-gray');
-const rainbowBtn = document.querySelector('#btn-rainbow');
-const eraseBtn = document.querySelector('#btn-erase');
-const resetBtn = document.querySelector('#btn-reset');
-
+let color = 'black';
+let click = true;
 
 function sketchPad(size) {
-
+    // create grid inside the container depends on the slider input
+    container.style.gridTemplateColumns = `repeat(${size}, 1fr)`;
+    container.style.gridTemplateRows = `repeat(${size}, 1fr)`;
+    
     container.innerHTML = '';
     
     for (let i = 0; i < size * size; i++) {
+        //create element div that will be put inside the container
         const divs = document.createElement('div');
-        container.style.gridTemplateColumns = `repeat(${size}, 1fr)`;
-        container.style.gridTemplateRows = `repeat(${size}, 1fr)`;
-        container.insertAdjacentElement('beforeend', divs);
-        divs.addEventListener('mouseover', function() {
-            this.classList.add('black');
-        });
+        // append the divs after the container
+        container.insertAdjacentElement("beforeend", divs);
+        divs.style.backgroundColor = 'white';
+        // on mouseover run setColor function that will change the bg color
+        divs.addEventListener('mouseover', useColor);
+
+        // reset button event listener
+        const resetBtn = document.querySelector('#btn-reset');
+        resetBtn.addEventListener('click', () => {
+            divs.style.backgroundColor = 'white';
+        })
     }
 }
 
@@ -36,17 +42,36 @@ function changeSize(val) {
 
 changeSize(16);
 
+// function will use the color on setColor and if the rainbow button is clicked will return random colors;
+function useColor() {
+    if (click) {
+        if (color === 'rainbow') {
+            this.style.backgroundColor = `hsl(${Math.random() * 360}, 100%, 50%)`; 
+        } 
+        else {
+            this.style.backgroundColor = color;
+        }
+    }
+}
 
-// color picker
-let setColor = document.querySelector('[type="color"]');
-setColor.addEventListener('input', (e) => {
-    color = e.target.value
+    // function that will set the color on button click or input choice
+function setColor(choice) {
+    color = choice;
+    colorPicker_span.textContent = `${choice}`;
+    colorPicker_span.style.textTransform = 'uppercase';
+    colorPicker_span.style.fontSize = '1.2rem';
+    colorPicker_span.style.fontWeight = '500';
+
+    if(choice === 'white') {
+        colorPicker_span.textContent = 'eraser';
+    }
+}
+
+document.body.addEventListener('click', function(){
+    if(click === true) {
+        click = false;
+    } else {
+        click = true;
+    }
 });
-
-/* TODO */
-// Buttons brush should be what ever the clicked button is
-// Button rainbow should be different colors
-// Whatever color picked on input color should be the brush color
-// Clear button should reset the sketchpad to white background
-// fix event listener from mouseover to toggle
 
